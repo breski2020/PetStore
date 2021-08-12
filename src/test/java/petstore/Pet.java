2ad3fd5437c8e342cec8c9f102f3931f9eec41f2
Guilherme <ramalhoguilherme@hotmail.com>
2021-08-11 22:28:03 -0300

    final do dia 2

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,4 +1,44 @@
+//1- Pacote
 package petstore;
 
+//2 - Bibliotecas
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+//3 - Classe
 public class Pet {
+    //3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; //Endereço da entidade pet
+
+    // 3.2 - Metodos (sem retorno VOID) e funções (com retorno)
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    //Incluir - Create - Post
+    @Test //Identificar metodo ou funcao como um teste para TestNG
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        //Sintaxe Gherkin
+        // Dado - Quando - Entao
+        // Given - When - Then
+
+        given() //Dado
+                .contentType("application/json") //comum em API REST - antigas era "test/xml"
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+    }
 }
